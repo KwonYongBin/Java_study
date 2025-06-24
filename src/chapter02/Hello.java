2aab3449d96f23390c353cd069bc8950236bb6bb
KwonYongBin <dydqls6789@naver.com>
2025-06-24 17:25:17 +0900

    .md file modifide commit

--- src/chapter02/Hello.java
+++ src/chapter02/Hello.java
@@ -3,8 +3,13 @@ package chapter02;
 public class Hello {
 	//main method
 	public static void main(String[] args) {
-		System.out.println("Welcome to JAVA!!");
+		//자바 환경에서 문자는 항상 double quotation 문잘르 감싸서 사용한다.
+		//(single quotation으로 감싸면 오류가 나타난다.)
+		//하지만 javascript에서는 둘다 사용해도 무관하다.
+		System.out.println("Welcome to JAVA!!"); 
 		System.out.println("Hello");
 		//ln은 line의 약자이다.
+		
+		//sysout(축약어) + ctrl + spacebar 단축기
 	}
 }
