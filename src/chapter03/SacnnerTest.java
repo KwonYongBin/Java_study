7f6c0aad133e4091feb7d1ad33c5e2b730dfea56
KwonYongBin <dydqls6789@naver.com>
2025-06-25 16:28:31 +0900

    .md file modifide commit

---
+++ src/chapter03/SacnnerTest.java
@@ -0,0 +1,26 @@
+package chapter03;
+
+import java.util.Scanner;
+
+public class SacnnerTest {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("이름 > ");
+		String name = scan.next();
+		
+		System.out.print("나이 > ");
+		int age = scan.nextInt();
+		
+		System.out.print("과목 > ");
+		String subject = scan.next();
+		
+		
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age);
+		System.out.println("과목 : " + subject);
+
+	}
+
+}
