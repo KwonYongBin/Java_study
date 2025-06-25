76f0fa38338f68b69e248d0a66cc80f61ea6c390
KwonYongBin <dydqls6789@naver.com>
2025-06-25 17:54:02 +0900

    .md file modifide commit

--- src/chapter03/Test.java
+++ src/chapter03/Test.java
@@ -6,13 +6,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("*****************************");
-		System.out.println("Welcome to Shopping Mall");
-		System.out.println("Welcome to Food Market");
-		System.out.println("*****************************");
-		System.out.println("1. 상품보기\t 2.상품보기");
-		System.out.println("3. 영수증보기\t 4.종료");
-		System.out.println("*****************************");
+		System.out.println("****************************************");
+		System.out.println("\tWelcome to Shopping Mall");
+		System.out.println("\tWelcome to Food Market");
+		System.out.println("****************************************");
+		System.out.println("\t1. 상품보기\t 2.상품보기");
+		System.out.println("\t3. 영수증보기\t 4.종료");
+		System.out.println("****************************************");
 		
 		Scanner menu = new Scanner(System.in);
 		System.out.print("메뉴를 선택해 주세요(숫자) > ");
