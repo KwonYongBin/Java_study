923737b13076bebcd3f27f1c7e8852b5a71fb8b8
KwonYongBin <dydqls6789@naver.com>
2025-08-04 12:24:21 +0900

    25.08.04

---
+++ src/chapter21_mini_project/GenericMarketRepositoryInterface.java
@@ -0,0 +1,5 @@
+package chapter21_mini_project;
+
+public interface GenericMarketRepositoryInterface {
+
+}
---
+++ src/chapter21_mini_project/application/MarketApplication.java
@@ -0,0 +1,10 @@
+package chapter21_mini_project.application;
+
+public class MarketApplication {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
